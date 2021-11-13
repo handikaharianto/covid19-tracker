97963aff626590eaa7369e289681e2c95b4acf63
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-11-13 22:25:15 +0700

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./sass/style.scss";
 
 import Map from "./components/Map/Map";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Map />
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header>
+      <div className="container">
+        <h1>
+          <a className="logo" href="/">
+            Covid 19 Tracker
+          </a>
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
