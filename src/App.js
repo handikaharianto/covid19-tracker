e8542c35b8442aad86dae2e5edffee95e5d4c8e1
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-11-15 05:58:16 +0700

    Add info to Markers

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import "./sass/style.scss";
 
 import Map from "./components/Map/Map";
 import Header from "./components/Header/Header";
+import useFetch from "./hooks/useFetch";
+
+const url = "https://disease.sh/v3/covid-19/countries";
 
 function App() {
+  const { data } = useFetch(url);
+
   return (
     <div className="App">
       <Header />
-      <Map />
+      <Map data={data} />
     </div>
   );
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,18 +1,52 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Popup, CircleMarker } from "react-leaflet";
 import "./Map.scss";
 
-function Map() {
+function Map({ data }) {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={2} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {data.map((singleCountry, index) => {
+        const {
+          country,
+          countryInfo: { lat, long, flag },
+          cases,
+          todayCases,
+          deaths,
+          recovered,
+          todayRecovered,
+        } = singleCountry;
+        return (
+          <CircleMarker
+            key={index}
+            center={[lat, long]}
+            color="#e63946"
+            stroke={false}
+            fillOpacity={0.8}
+            radius={15}
+          >
+            <Popup>
+              <div className="country-details">
+                <img
+                  className="country-details__flag"
+                  src={flag}
+                  alt={country}
+                ></img>
+                <h5 className="country-details__name">{country}</h5>
+              </div>
+              <ul className="country-info">
+                <li>Total Cases: {cases}</li>
+                <li>Total Recovered: {recovered}</li>
+                <li>Total Deaths: {deaths}</li>
+                <li>Total New Cases Today: {todayCases}</li>
+                <li>Total Recovered Today: {todayRecovered}</li>
+              </ul>
+            </Popup>
+          </CircleMarker>
+        );
+      })}
     </MapContainer>
   );
 }
