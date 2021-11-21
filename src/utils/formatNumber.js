6185d8d0274677d2c74fc837380502ca8603a6c9
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-11-21 20:26:20 +0700

    Add function to format numbers

---
+++ src/utils/formatNumber.js
@@ -0,0 +1,9 @@
+function formatNumber(num) {
+  if (num < 1e3) return num;
+  if (num >= 1e3 && num < 1e6) return +(num / 1e3).toFixed(1) + "K";
+  if (num >= 1e6 && num < 1e9) return +(num / 1e6).toFixed(1) + "M";
+  if (num >= 1e9 && num < 1e12) return +(num / 1e9).toFixed(1) + "B";
+  if (num >= 1e12) return +(num / 1e12).toFixed(1) + "T";
+}
+
+export default formatNumber;
