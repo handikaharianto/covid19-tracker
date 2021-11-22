b94ffe6b16c0c2af1ba5faec8255cf3743572c77
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-11-22 20:22:34 +0700

    Add Loader and Message components to Stats section

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -5,11 +5,13 @@ import { MdGroups, MdAdd } from "react-icons/md";
 import { FaSkullCrossbones } from "react-icons/fa";
 import { GiHealthNormal } from "react-icons/gi";
 import formatNumber from "../../utils/formatNumber";
+import Loader from "../Loader/Loader";
+import Message from "../Message/Message";
 
 const url = "https://disease.sh/v3/covid-19/all";
 
 function Stats() {
-  const { data } = useFetch(url);
+  const { data, loading, error } = useFetch(url);
 
   const {
     updated,
@@ -24,30 +26,38 @@ function Stats() {
 
   return (
     <section className="stats">
-      <StatsCard
-        total={formatNumber(population)}
-        updates={"Last Updated: " + new Date(updated).toDateString()}
-        statsName="Population"
-        icons={<MdGroups />}
-      />
-      <StatsCard
-        total={formatNumber(cases)}
-        updates={`+${formatNumber(todayCases)} New Cases`}
-        statsName="Total Cases"
-        icons={<MdAdd />}
-      />
-      <StatsCard
-        total={formatNumber(deaths)}
-        updates={`+${formatNumber(todayDeaths)} New Deaths`}
-        statsName="Total Deaths"
-        icons={<FaSkullCrossbones />}
-      />
-      <StatsCard
-        total={formatNumber(recovered)}
-        updates={`+${formatNumber(todayRecovered)} New Recovery`}
-        statsName="Total Recovered"
-        icons={<GiHealthNormal />}
-      />
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message message={error} />
+      ) : (
+        <>
+          <StatsCard
+            total={formatNumber(population)}
+            updates={"Last Updated: " + new Date(updated).toDateString()}
+            statsName="Population"
+            icons={<MdGroups />}
+          />
+          <StatsCard
+            total={formatNumber(cases)}
+            updates={`+${formatNumber(todayCases)} New Cases`}
+            statsName="Total Cases"
+            icons={<MdAdd />}
+          />
+          <StatsCard
+            total={formatNumber(deaths)}
+            updates={`+${formatNumber(todayDeaths)} New Deaths`}
+            statsName="Total Deaths"
+            icons={<FaSkullCrossbones />}
+          />
+          <StatsCard
+            total={formatNumber(recovered)}
+            updates={`+${formatNumber(todayRecovered)} New Recovery`}
+            statsName="Total Recovered"
+            icons={<GiHealthNormal />}
+          />
+        </>
+      )}
     </section>
   );
 }
