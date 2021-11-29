daffc56d3340a4af12ef3ee4422078091e623c38
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-11-29 20:24:13 +0700

    Add Routing to separate Dashboard & Tables

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Stats from "./components/Stats/Stats";
 import Graph from "./components/Graph/Graph";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [isSidebarActive, setIsSidebarActive] = useState(false);
@@ -14,22 +15,34 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <main>
-        <Header
-          setIsSidebarActive={setIsSidebarActive}
-          handleClick={handleClick}
-        />
-        <Stats />
-        <Graph />
-        <Map />
-      </main>
-      <Sidebar
-        isSidebarActive={isSidebarActive}
-        setIsSidebarActive={setIsSidebarActive}
-        handleClick={handleClick}
-      />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <main>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Header handleClick={handleClick} title="dashboard" />
+                  <Stats />
+                  <Graph />
+                  <Map />
+                </>
+              }
+            />
+            <Route
+              path="/tables"
+              element={
+                <>
+                  <Header handleClick={handleClick} title="tables" />
+                </>
+              }
+            />
+          </Routes>
+        </main>
+        <Sidebar isSidebarActive={isSidebarActive} handleClick={handleClick} />
+      </div>
+    </BrowserRouter>
   );
 }
 
