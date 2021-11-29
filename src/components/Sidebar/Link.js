92fdce7c78183acec1b3a67a505697dc7c90344d
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-11-29 20:15:22 +0700

    Add Active Link to Link component

--- src/components/Sidebar/Link.js
+++ src/components/Sidebar/Link.js
@@ -1,13 +1,17 @@
+import { NavLink } from "react-router-dom";
 import "./Link.scss";
 
 function Link({ url, name, Icon }) {
   return (
-    <li className="sidebar-nav__link">
-      <a href={url}>
-        {<Icon />}
-        {name}
-      </a>
-    </li>
+    <NavLink
+      to={url}
+      className={({ isActive }) =>
+        "sidebar-nav__link " + (isActive ? "sidebar-nav__link--active" : "")
+      }
+    >
+      {<Icon />}
+      {name}
+    </NavLink>
   );
 }
 
